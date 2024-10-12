ca0967845125b3d594c7ad668a68ef533b610aa1
--global <toritosalgado90@gmail.com>
2024-10-12 15:07:30 -0700

    idk

--- users/sunshine-host/home/common.nix
+++ users/sunshine-host/home/common.nix
@@ -2,7 +2,6 @@
   imports = [
     # import programs here
     ../../modules/core
-    ../../modules/optional/dev/btop.nix
   ];
 
   home = {
@@ -10,16 +9,6 @@
     homeDirectory = lib.mkDefault "/home/sunshine-host";
   };
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
   home.stateVersion = "23.11";
-
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
