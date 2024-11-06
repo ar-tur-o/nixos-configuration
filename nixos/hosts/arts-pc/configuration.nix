77c1ef2f65f75c8302550adfa3c575fed6fcd733
artisal@arts-pc <toritosalgado90@gmail.com>
2024-11-05 19:17:54 -0800

    enabled flatpak only on pc

--- nixos/hosts/arts-pc/configuration.nix
+++ nixos/hosts/arts-pc/configuration.nix
@@ -21,6 +21,8 @@
     # ./raid-1.nix
   ];
 
+  services.flatpak.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
